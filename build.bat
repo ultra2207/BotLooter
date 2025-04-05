03c21ea3f8de3afb6ac77e152f3554e76f61e82e
ultra2207 <119686777+ultra2207@users.noreply.github.com>
2025-04-06 00:20:10 +0530

    added a build file and changed steamusersession to english

---
+++ build.bat
@@ -0,0 +1,3 @@
+dotnet publish -r win-x64 -c Release --self-contained true /p:PublishSingleFile=true
+
+:: Builds the botlooter exe and stores it here: "C:\Users\Sivasai\Documents\GitHub\BotLooter\BotLooter\bin\Release\net7.0\win-x64\publish\BotLooter.exe"
